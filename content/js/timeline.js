c0ca4ad565b07e023c2d206dfee193f34ec2a683
qatron <patryk.st01@gmail.com>
2017-10-12 12:50:47 +0200

    Timeline parts initialization

--- content/js/timeline.js
+++ content/js/timeline.js
@@ -2,5 +2,37 @@
 $(function(){
 	"use strict";
 	
+	class Part {
+		constructor(element) {
+			this._element = element;
+		}
+		
+		get element() {
+			return this._element;
+		}
+		set element(item) {
+			this._element = item;
+		}
+		
+		get flexGrow() {
+			return this._element.css('flex-grow');
+		}
+		set flexGrow(val) {
+			this._element.css('flex-grow', val);
+		}
+	}
+	
+	
+	let timeline = {
+		part: [],
+		masterPoint: []
+	};
+	
+	for(let parts = $('.part'), q = 0; q < parts.length; q++) {
+		timeline.part.push(new Part(parts.eq(q)));
+	}	
+	
+	
 	console.log('loaded');
-})();
+	
+});
