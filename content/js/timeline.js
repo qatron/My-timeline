ca4eca60b71cc3f2e7f0f9173c963fa129ada89b
qatron <patryk.st01@gmail.com>
2017-10-13 21:02:38 +0200

    Timeline elements basic init

--- content/js/timeline.js
+++ content/js/timeline.js
@@ -7,13 +7,6 @@ $(function(){
 			this._element = element;
 		}
 		
-		get element() {
-			return this._element;
-		}
-		set element(item) {
-			this._element = item;
-		}
-		
 		get flexGrow() {
 			return this._element.css('flex-grow');
 		}
@@ -24,13 +17,42 @@ $(function(){
 	
 	
 	let timeline = {
-		part: [],
-		masterPoint: []
+		
+		_parts: [],
+		partsInit() {
+			for(let parts = $('.part'), q = 0; q < parts.length; q++) {
+				timeline._parts.push(new Part(parts.eq(q)));
+			}
+		},
+		
+		_activePart: undefined,
+		partActivate(index) {
+			let activePart = timeline._activePart;
+			if(activePart) {
+				activePart.flexGrow = 0;
+			}
+			activePart = timeline._parts[index];
+			activePart.flexGrow = 1;
+			timeline._activePart = activePart;
+		},
+		
+		_masterPoints: [],
+		masterPointsInit() {
+			let items = $('.master-point');
+			for(let q = 0; q<items.length; q++) {
+				timeline._masterPoints.push(items.eq(q));
+			}
+			
+			items.click((e)=>{
+				let that = $(e.currentTarget);
+				timeline.partActivate(that.index()/2);
+			});
+			
+		}
 	};
 	
-	for(let parts = $('.part'), q = 0; q < parts.length; q++) {
-		timeline.part.push(new Part(parts.eq(q)));
-	}	
+	timeline.partsInit();
+	timeline.masterPointsInit();
 	
 	
 	console.log('loaded');
